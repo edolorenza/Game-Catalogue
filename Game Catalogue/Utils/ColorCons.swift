0c30b7c1f4a82e96d29e3e9f2748f93e6d23cd67
edo Lorenza <edolrnz@gmail.com>
2021-06-07 09:25:30 +0700

    setup tabBar controller

---
+++ Game Catalogue/Utils/ColorCons.swift
@@ -0,0 +1,13 @@
+//
+//  ColorCons.swift
+//  Game Catalogue
+//
+//  Created by Edo Lorenza on 07/06/21.
+//
+
+import UIKit
+
+class ColorCons {
+    static let balticSea = #colorLiteral(red: 0.1775007546, green: 0.1720837057, blue: 0.1896696687, alpha: 1)
+    static let tuna = #colorLiteral(red: 0.1802419126, green: 0.1919552684, blue: 0.2266210616, alpha: 1)
+}
